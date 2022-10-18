5c8db47f82b01ebf7eec39cee27eecf33e923379
Eduardo Souza <edcampossouza@gmail.com>
2022-10-18 14:37:02 -0300

    gera cartas embaralhadas

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const parrots = [
 ];
 
 const opcoes = {};
+const arrayImagens = [];
+const container = document.querySelector(".container");
 
 function selecionaNumeroCartas() {
   let num = 0;
@@ -22,6 +24,62 @@ function selecionaNumeroCartas() {
   opcoes.numeroCartas = num;
 }
 
+function carregaCartas() {
+  for (let i = 0; i < arrayImagens.length; i++) {
+    const imgParrot = document.createElement("img");
+    imgParrot.src = `./images/${arrayImagens[i]}`;
+    const imgPadrao = document.createElement("img");
+    imgPadrao.src = "./images/back.png";
+    //card
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    //face (frente)
+    const frente = document.createElement("div");
+    frente.classList.add("face");
+    frente.classList.add("front-face");
+    frente.appendChild(imgPadrao);
+
+    //face (verso)
+    const verso = document.createElement("div");
+    verso.classList.add("face");
+    verso.classList.add("back-face");
+    verso.appendChild(imgParrot);
+
+    //insere as duas faces no card
+    card.appendChild(frente);
+    card.appendChild(verso);
+
+    card.onclick = function () {
+      viraCarta(card);
+    };
+
+    container.appendChild(card);
+  }
+}
+
+function inicializaCartas() {
+  for (let i = 0; i < opcoes.numeroCartas / 2; i++) {
+    arrayImagens.push(parrots[i]);
+    arrayImagens.push(parrots[i]);
+  }
+
+  //embaralha as cartas
+  arrayImagens.sort(function () {
+    return Math.random() - 0.5;
+  });
+
+  carregaCartas();
+}
+
+function viraCarta(carta) {
+  const back = carta.querySelector(".back-face");
+  const front = carta.querySelector(".front-face");
+  back.classList.toggle("show");
+  front.classList.toggle("hide");
+}
+
 //inicio script
 
 selecionaNumeroCartas();
+inicializaCartas();
